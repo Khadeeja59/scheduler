e835e2440218c325884d884c5f881dbe6e9450e7
Khadeeja <kkausar59@gmail.com>
2022-01-19 19:08:05 -0500

    Form component

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import Button from "../Button";
+import InterviewerList from "../InterviewerList";
+export default function Form(props){
+  const [name, setName] = useState(props.name || "");  //[studentn,setStudent]
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const reset = () => {
+    setName("");
+    setInterviewer("");
+  };
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+  const save = () => {
+    props.onSave(name, interviewer);
+  }
+  return (
+    <main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+    <form autoComplete="off">
+      <input
+        className="appointment__create-input text--semi-bold"
+        name="name"
+        type="text"
+        placeholder="Enter Student Name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+       
+      />
+    </form>
+    <InterviewerList 
+    interviewers={props.interviewers}
+    value={interviewer} 
+    onChange={setInterviewer} 
+   />
+  </section>
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button danger onClick={cancel}>Cancel</Button>
+      <Button confirm onClick={save}>Save</Button>
+    </section>
+  </section>
+</main>
+
+  
+  
+  )
+};
--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -165,6 +166,22 @@ storiesOf("InterviewerList", module)
   ))
   .add("Status", () => <Status message="Deleting"/>)
   .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
+  .add("Edit form", () => (
+    <Form 
+      name="Lydia Miller-Jones"
+      interviewer={3}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create form", () => (
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
 
 
 
